79c19a3698d24a057deb645eb07c86dd75e3bb16
LucasAlfare <god-lucas@hotmail.com>
2024-06-10 23:30:17 -0300

    includes alternative cube state handling class

--- src/rc/Cube.ts
+++ src/rc/Cube.ts
@@ -43,42 +43,42 @@ class Cube {
   faces = ['U', 'R', 'F', 'D', 'L', 'B'];
 
   /**
-   * Target indexes that should be permuted to represent a "U move".
+   * Target indexes that should be permuted to represent a "U move". The difference to the solved state is the actual permutation indexes.
    */
   U_Move: Array<number> = [
     6, 3, 0, 7, 4, 1, 8, 5, 2, 45, 46, 47, 12, 13, 14, 15, 16, 17, 9, 10, 11, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 18, 19, 20, 39, 40, 41, 42, 43, 44, 36, 37, 38, 48, 49, 50, 51, 52, 53
   ];
 
   /**
-   * Target indexes that should be permuted to represent a "R move".
+   * Target indexes that should be permuted to represent a "R move". The difference to the solved state is the actual permutation indexes.
    */
   R_Move: Array<number> = [
     0, 1, 20, 3, 4, 23, 6, 7, 26, 15, 12, 9, 16, 13, 10, 17, 14, 11, 18, 19, 29, 21, 22, 32, 24, 25, 35, 27, 28, 51, 30, 31, 48, 33, 34, 45, 36, 37, 38, 39, 40, 41, 42, 43, 44, 8, 46, 47, 5, 49, 50, 2, 52, 53
   ];
 
   /**
-   * Target indexes that should be permuted to represent a "F move".
+   * Target indexes that should be permuted to represent a "F move". The difference to the solved state is the actual permutation indexes.
    */
   F_Move: Array<number> = [
     0, 1, 2, 3, 4, 5, 44, 41, 38, 6, 10, 11, 7, 13, 14, 8, 16, 17, 24, 21, 18, 25, 22, 19, 26, 23, 20, 15, 12, 9, 30, 31, 32, 33, 34, 35, 36, 37, 27, 39, 40, 28, 42, 43, 29, 45, 46, 47, 48, 49, 50, 51, 52, 53
   ];
 
   /**
-   * Target indexes that should be permuted to represent a "D move".
+   * Target indexes that should be permuted to represent a "D move". The difference to the solved state is the actual permutation indexes.
    */
   D_Move: Array<number> = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 24, 25, 26, 18, 19, 20, 21, 22, 23, 42, 43, 44, 33, 30, 27, 34, 31, 28, 35, 32, 29, 36, 37, 38, 39, 40, 41, 51, 52, 53, 45, 46, 47, 48, 49, 50, 15, 16, 17
   ];
 
   /**
-   * Target indexes that should be permuted to represent a "L move".
+   * Target indexes that should be permuted to represent a "L move". The difference to the solved state is the actual permutation indexes.
    */
   L_Move: Array<number> = [
     53, 1, 2, 50, 4, 5, 47, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0, 19, 20, 3, 22, 23, 6, 25, 26, 18, 28, 29, 21, 31, 32, 24, 34, 35, 42, 39, 36, 43, 40, 37, 44, 41, 38, 45, 46, 33, 48, 49, 30, 51, 52, 27
   ];
 
   /**
-   * Target indexes that should be permuted to represent a "B move".
+   * Target indexes that should be permuted to represent a "B move". The difference to the solved state is the actual permutation indexes.
    */
   B_Move: Array<number> = [
     11, 14, 17, 3, 4, 5, 6, 7, 8, 9, 10, 35, 12, 13, 34, 15, 16, 33, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 36, 39, 42, 2, 37, 38, 1, 40, 41, 0, 43, 44, 51, 48, 45, 52, 49, 46, 53, 50, 47
