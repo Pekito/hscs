b6b3c0213a2f1b1af9ec6730097da28797c00b0e
Pedro Luiz <p3dr0c4@live.com>
2024-06-12 23:00:20 -0300

    feat: add method to check if bottom cross is solved

--- src/analyzers/CommonAnalyzers.ts
+++ src/analyzers/CommonAnalyzers.ts
@@ -1,6 +1,6 @@
-import { CENTER_FACE_INDEX, FACE_INDEX_ARRAY } from "../cube/Constants";
+import { BOTTOM_EDGE_STICKER_INDEX, CENTER_FACE_INDEX, D_LAYER_INDEX, EDGES_STICKER_INDEXES, FACE_INDEX_ARRAY, Y_AXIS_LAYERS } from "../cube/Constants";
 import { getCenterIndex, getFace } from "../cube/Cube";
-import { RubiksCube } from "../cube/Types";
+import { RubiksCube, RubiksCubeFace } from "../cube/Types";
 
 export const isFaceSolved = (faceIndex: number, cube: RubiksCube): boolean => {
     const face = getFace(faceIndex, cube);
@@ -11,4 +11,16 @@ export const isFaceSolved = (faceIndex: number, cube: RubiksCube): boolean => {
 
 export const isCubeSolved = (cube: RubiksCube) => {
     return FACE_INDEX_ARRAY.every((faceIndex) => isFaceSolved(faceIndex, cube));
+}
+export const getFaceEdgesIndex = (face: RubiksCubeFace) => EDGES_STICKER_INDEXES.map(edgeIndex => face[edgeIndex]);
+export const areFaceEdgesSolved = (face: RubiksCubeFace): boolean => EDGES_STICKER_INDEXES.every(edgeIndex => {
+    const isHorizontalSolved = Math.abs(face[CENTER_FACE_INDEX] - face[edgeIndex]) === 1;
+    const isVerticalSolved = Math.abs(face[CENTER_FACE_INDEX] - face[edgeIndex]) === 3;
+    return isHorizontalSolved || isVerticalSolved;
+});
+export const isBottomCrossSolved = (cube: RubiksCube) => {
+    const isBottomEdgeStickerSolved = (faceIndex: number) => getCenterIndex(faceIndex, cube) + 3 === getFace(faceIndex, cube)[BOTTOM_EDGE_STICKER_INDEX];
+    const areBottomEdgesSolved = Y_AXIS_LAYERS.every(isBottomEdgeStickerSolved);
+    const areDLayerEdgesSolved = areFaceEdgesSolved(getFace(D_LAYER_INDEX, cube));
+    return areBottomEdgesSolved && areDLayerEdgesSolved;
 }
--- tests/analyzers/CommonAnalyzers.test.ts
+++ tests/analyzers/CommonAnalyzers.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, it } from "vitest";
 import { createCube, moveCube } from "../../src/cube/Cube";
-import { isCubeSolved } from "../../src/analyzers/CommonAnalyzers";
+import { isBottomCrossSolved, isCubeSolved } from "../../src/analyzers/CommonAnalyzers";
 import { R_CLOCKWISE_MOVE, Z_CLOCKWISE_MOVE } from "../../src/cube/Moves";
+import { applySequence } from "../../src/cube/Notation";
+import { RubiksCube } from "../../src/cube/Types";
 
 describe("isCubeSolved", () => {
     it("Should return true if cube is solved", () => {
@@ -17,3 +19,14 @@ describe("isCubeSolved", () => {
         expect(isCubeSolved(turnedCube)).toEqual(false);
     });
 });
+
+describe("isBottomCrossSolved", () => {
+    it("Should return true if cross is solved", () => {
+        const solvedCrossCube = applySequence(createCube(), "R U R' U'") as RubiksCube;
+        expect(isBottomCrossSolved(solvedCrossCube)).toEqual(true);
+    });
+    it("Should return false if bottom cross is not solved", () => {
+        const solvedCrossCube = applySequence(createCube(), "D2") as RubiksCube;
+        expect(isBottomCrossSolved(solvedCrossCube)).toEqual(false);
+    }) 
+})
