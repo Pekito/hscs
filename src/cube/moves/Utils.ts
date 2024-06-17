5090f2925c5d702ea94579a4984d5e9a026cc908
Pedro Luiz <p3dr0c4@live.com>
2024-06-17 17:22:21 -0300

    feat: create method to mirror moves and sequences

--- src/cube/moves/Utils.ts
+++ src/cube/moves/Utils.ts
@@ -1,4 +1,6 @@
 import { areArraysEqual } from "../../Utils";
+import { CENTER_INDEXES_ARRAY, NUMBER_OF_STICKERS_ON_CUBE } from "../Constants";
+import { getNotationFromMove, notationToMoveMap } from "../Notation";
 import { RubiksCubeLayerMoveGroup, RubiksCubeMove } from "../Types";
 
 export const isMoveFromLayer = (move: RubiksCubeMove, layerMoves: RubiksCubeLayerMoveGroup) => {
@@ -10,3 +12,10 @@ export const isMoveDouble = (move: RubiksCubeMove, layer: RubiksCubeLayerMoveGro
 export const areMovesMirrored = (move1: RubiksCubeMove, move2: RubiksCubeMove): boolean => {
     return move1.every((pos, idx) => move2[pos] === idx);
 }
+export const mirrorMove = (move: RubiksCubeMove) => {
+    const notation = getNotationFromMove(move);
+    if(notation.includes("2")) return move;
+    if(notation.includes("'")) return notationToMoveMap[notation.replace("'", "")]
+    return notationToMoveMap[notation + "'"]
+};
+export const mirrorSequence = (sequence:  RubiksCubeMove[]) => sequence.map(mirrorMove).reverse();
--- tests/moves/Utils.test.ts
+++ tests/moves/Utils.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from "vitest";
-import { areMovesMirrored, R_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE, U_DOUBLE_MOVE } from "../../src/cube/moves";
+import { areMovesMirrored, D_CLOCKWISE_MOVE, D_COUNTER_CLOCKWISE_MOVE, mirrorMove, mirrorSequence, R_CLOCKWISE_MOVE, R_COUNTER_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE, U_DOUBLE_MOVE } from "../../src/cube/moves";
+import { areArraysEqual } from "../../src/Utils";
+import { getNotationFromMove } from "../../src/cube/Notation";
 
 describe("areMovesMirrored", () => {
     it("Should return true", () => {
@@ -15,4 +17,29 @@ describe("areMovesMirrored", () => {
         expect(areMovesMirrored(U_DOUBLE_MOVE, U_COUNTER_CLOCKWISE_MOVE)).toEqual(false);
         expect(areMovesMirrored(U_DOUBLE_MOVE, R_CLOCKWISE_MOVE)).toEqual(false);
     })
+})
+describe("mirrorMove", () => {
+    it("Should mirror a clockwise quarter move correctly", () => {
+        const mirroredUClockwise = mirrorMove(U_CLOCKWISE_MOVE);
+        expect(areArraysEqual(U_COUNTER_CLOCKWISE_MOVE, mirroredUClockwise)).toEqual(true);
+    });
+    it("Should mirror a counter clockwise quarter move correctly", () => {
+        const mirroredUCounterClockwise = mirrorMove(U_COUNTER_CLOCKWISE_MOVE);
+        expect(areArraysEqual(U_CLOCKWISE_MOVE, mirroredUCounterClockwise)).toEqual(true);
+    });
+    it("Should mirror a double move correctly", () => {
+        const targetMove = U_DOUBLE_MOVE;
+        const uDoubleMirrored = mirrorMove(U_DOUBLE_MOVE);
+        console.log(getNotationFromMove(targetMove), getNotationFromMove(uDoubleMirrored));
+        expect(areArraysEqual(targetMove, U_DOUBLE_MOVE)).toEqual(true);
+    })
+});
+
+describe("mirrorSequence", () => {
+    it("Should mirror a sequence", () => {
+        const sequence = [R_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, D_COUNTER_CLOCKWISE_MOVE];
+        const expected = [D_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE, R_COUNTER_CLOCKWISE_MOVE];
+        const mirroredSequence = mirrorSequence(sequence);
+        expect(expected).toEqual(mirroredSequence);
+    }) 
 })
