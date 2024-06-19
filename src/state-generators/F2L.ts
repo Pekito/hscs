06839021af390f17b2590fc9a3dae338724c25e7
Pedro Luiz <p3dr0c4@live.com>
2024-06-19 17:11:06 -0300

    feat: improve f2l state performance

--- src/state-generators/F2L.ts
+++ src/state-generators/F2L.ts
@@ -1,10 +1,12 @@
+import { Console } from "console";
 import { isBottomCrossSolved } from "../analyzers/CommonAnalyzers";
 import { createCube, createCubeState } from "../cube/Cube";
-import { F_CLOCKWISE_MOVE, F_COUNTER_CLOCKWISE_MOVE, getAllSequenceMirrors, LAYER_MOVES_ARRAY, mirrorMoveHorizontally, mirrorMoveToBack, mirrorSequenceHorizontally, mirrorSequenceToBack, R_CLOCKWISE_MOVE, R_COUNTER_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE } from "../cube/moves";
+import { F_CLOCKWISE_MOVE, F_COUNTER_CLOCKWISE_MOVE, getAllSequenceMirrors, LAYER_MOVES_ARRAY, mirrorMoveHorizontally, mirrorMoveToBack, mirrorSequenceHorizontally, mirrorSequenceToBack, R_CLOCKWISE_MOVE, R_COUNTER_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE, U_DOUBLE_MOVE } from "../cube/moves";
 import { mapNotationArrayToMove } from "../cube/Notation";
 import { RubiksCube } from "../cube/Types";
 import { StateHashTableKeyCreator } from "../solvers/DataStructures";
 import { findStatesWithOptimalSolution } from "../solvers/Finders";
+import { iterativeDepthSearchSolution } from "../solvers/SearchAlgorithms";
 import { print2DCube, printWCACube } from "../visualizers/PrintCube";
 import { getSolvedCubeOrientation, SOLVED_STATES_TABLE } from "./SolvedState";
 
@@ -17,9 +19,20 @@ const pescaR = getAllSequenceMirrors([R_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, R_COUN
 const inserR = getAllSequenceMirrors([R_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE, R_COUNTER_CLOCKWISE_MOVE]);
 const pescaF = getAllSequenceMirrors([F_COUNTER_CLOCKWISE_MOVE, U_CLOCKWISE_MOVE, F_CLOCKWISE_MOVE]);
 const inserF = getAllSequenceMirrors([F_COUNTER_CLOCKWISE_MOVE, U_COUNTER_CLOCKWISE_MOVE, F_CLOCKWISE_MOVE]);
-const FRONT_RIGHT_F2L_STATES = () => findStatesWithOptimalSolution({
+const meiaLuaR = getAllSequenceMirrors([R_CLOCKWISE_MOVE, U_DOUBLE_MOVE, R_COUNTER_CLOCKWISE_MOVE]);
+const meiaLuaF = getAllSequenceMirrors([F_COUNTER_CLOCKWISE_MOVE, U_DOUBLE_MOVE, F_CLOCKWISE_MOVE])
+const FRONT_RIGHT_F2L_FREE_SLOT_STATES = () => findStatesWithOptimalSolution({
     cubeStateNode: createCube(),
-    maxDepth: 5,
-    possibleMoves: [...pescaR, ...inserR, ...pescaF, ...inserF],
+    maxDepth: 4,
+    possibleMoves: [
+        U_CLOCKWISE_MOVE, 
+        U_DOUBLE_MOVE, 
+        U_COUNTER_CLOCKWISE_MOVE, 
+        ...pescaR, 
+        ...inserR, 
+        ...pescaF, 
+        ...inserF, 
+        ...meiaLuaF, 
+        ...meiaLuaR],
     stateKeyCreator: createFrontRightF2LStateKey,
 });
