dc5adbf48f8e74bbcd533b0070bffcf14236df52
Pedro Luiz <p3dr0c4@live.com>
2024-06-22 20:39:25 -0300

    feat: populate states table

--- src/db/database-scripts/main.ts
+++ src/db/database-scripts/main.ts
@@ -1,12 +1,18 @@
 import BottomCross from "../states/BottomCross";
 import F2L from "../states/F2L";
+import OLL from "../states/OLL";
+import PLL from "../states/PLL";
 
 (async () => {
     console.log("Creating Tables");
      BottomCross.createStatesTable();
      F2L.createStatesTable();
+     OLL.createStatesTable();
+     PLL.createStatesTable();
      console.log("Populating Tables");
      BottomCross.populateStatesTable();
      F2L.populateStatesTable();
+     OLL.populateStatesTable();
+     PLL.populateStatesTable();
     console.log("Finished :)")
 })();
