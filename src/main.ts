95bcd8c250b227ef6f7453fdc1e5b30b2388aa56
Pedro Luiz <p3dr0c4@live.com>
2024-06-22 20:39:32 -0300

    feat: add scramble demonstration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-import { startMainThread } from "./state-generators/bottom-cross/mainStateSolver";
+import { createCubeState } from "./cube/Cube";
+import { mapNotationSequenceToMoveSequence } from "./cube/Notation";
+import CFOP from "./solvers/CFOP";
+import { printWCACube } from "./visualizers/PrintCube";
+import { joinMovesBySpaces } from "./visualizers/PrintMove";
 
-startMainThread();
+const scramble = "U2 R2 U2 R2 B' D2 B R2 B D2 B2 F' U B R' D' R F L' D2 R2";
+console.log(`>> Scramble: ${scramble} << \n`)
+const scrambledCube = createCubeState(mapNotationSequenceToMoveSequence(scramble));
+
+const solve = CFOP.solveCube(scrambledCube);
+const solvedCube = createCubeState(solve.completeSolution, scrambledCube);
+
+
+printWCACube(solvedCube, "Cubo resolvido 🎉");
+console.log("\n");
+console.log(`Solução da Cruz: ${joinMovesBySpaces(solve.crossSolution)} \n`);
+console.log(`Solução do F2L: ${joinMovesBySpaces(solve.f2lSolution)} \n`);
+console.log(`Solução da OLL: ${joinMovesBySpaces(solve.ollSolution)} \n`);
+console.log(`Solução da PLL: ${joinMovesBySpaces(solve.pllSolution)}`);
