23a4297563c8384d78221b82318785c92ec6c9f3
Pedro Luiz <p3dr0c4@live.com>
2024-06-24 08:52:07 -0300

    refactor: change parse notation method name

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createCubeState } from "./cube/Cube";
-import { mapNotationSequenceToMoveSequence } from "./cube/Notation";
+import { parseNotationSequenceToMoveSequence } from "./cube/Notation";
 import CFOP from "./solvers/CFOP";
 import { printWCACube } from "./visualizers/PrintCube";
 import { joinMovesBySpaces } from "./visualizers/PrintMove";
 
 const scramble = "U2 R2 U2 R2 B' D2 B R2 B D2 B2 F' U B R' D' R F L' D2 R2";
 console.log(`>> Scramble: ${scramble} << \n`)
-const scrambledCube = createCubeState(mapNotationSequenceToMoveSequence(scramble));
+const scrambledCube = createCubeState(parseNotationSequenceToMoveSequence(scramble));
 
 const solve = CFOP.solveCube(scrambledCube);
 const solvedCube = createCubeState(solve.completeSolution, scrambledCube);
